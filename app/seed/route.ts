5a3125ee825aca62c8b5af8091ff5a3f66b28605
Cristian Arias <cristian.arias.chaves@hotmail.com>
2025-08-04 10:18:42 -0600

    Update project

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -4,6 +4,8 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+console.log(sql)
+
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
